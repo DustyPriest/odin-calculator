55fa5c4425dec12fef91d570562d134bbe156635
DustyPriest <hugo.a.obe@gmail.com>
2022-05-21 14:59:11 +0800

    operator display, equation array storage

--- script.js
+++ script.js
@@ -5,27 +5,51 @@
 
 const DEFAULT_DISPLAY = '0';
 
-let mainDisplay = DEFAULT_DISPLAY;
-let subDisplay = '';
+let mainDisplay = [DEFAULT_DISPLAY];
+let subDisplay = [];
 
 /* ------ ELEMENT SELECTORS ------ */
 
 const mainDisplayEl = document.querySelector('.main-display');
-const subDuisplayEl = document.querySelector('.sub-display');
+const subDisplayEl = document.querySelector('.sub-display');
 const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
 
 /* ------ EVENT LISTENERS ------ */
 
 digits.forEach((digit) => {
-  digit.addEventListener('click', storeDisplay);
+  digit.addEventListener('click', inputDigit);
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', inputOperator);
 });
 
 /* ------ LISTENER FUNCTIONS------ */
 
-function storeDisplay(e) {
-  mainDisplay === '0'
-    ? (mainDisplay = e.target.textContent)
-    : (mainDisplay = mainDisplay + e.target.textContent);
+function inputDigit(e) {
+  // replace default display
+  // or begin new number
+  // or add to current number
+  if (mainDisplay[mainDisplay.length - 1] === DEFAULT_DISPLAY)
+    mainDisplay[0] = e.target.textContent;
+  else if (isNaN(mainDisplay[mainDisplay.length - 1]))
+    mainDisplay.push(e.target.textContent);
+  else mainDisplay[mainDisplay.length - 1] += e.target.textContent;
+
+  updateMainDisplay();
+}
+
+function inputOperator(e) {
+  // remove current operator if one already selected
+  // insert 0 if necessary
+  // insert new operator
+  if (isNaN(mainDisplay[mainDisplay.length - 1])) {
+    mainDisplay.pop();
+    if (mainDisplay.length === 0) mainDisplay.push('0');
+  }
+
+  mainDisplay.push(e.target.textContent);
 
   updateMainDisplay();
 }
@@ -33,7 +57,7 @@ function storeDisplay(e) {
 /* ------ ADDITIONAL FUNCTIONS ------ */
 
 const updateMainDisplay = () => {
-  mainDisplayEl.textContent = mainDisplay;
+  mainDisplayEl.textContent = mainDisplay.join(' ');
 };
 
 const add = (a, b) => {
